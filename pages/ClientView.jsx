32fbbe3988345939102e1238760a3483d1167806
WOO JIN SUL <99050264+ericsul@users.noreply.github.com>
2024-03-21 15:33:58 -0600

    Update ClientView.jsx

--- pages/ClientView.jsx
+++ pages/ClientView.jsx
@@ -1,5 +1,4 @@
-
-           import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from "../../_utils/firebase";
 import { useRouter } from "next/router";
 import ChatRoom from "./ChatRoom"; // Import ChatRoom component

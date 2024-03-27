4be0d6a4309d65a3b80d19af0a1cddce00265f08
WeiDai15 <wei.dai@edu.sait.ca>
2024-03-27 15:02:49 -0600

    Update fileList.jsx

--- pages/components/fileList.jsx
+++ pages/components/fileList.jsx
@@ -55,8 +55,8 @@ const FileList = ({ projectId}) => {
 
   return (
     <div >
-      <div style={{ backgroundColor: '#E3E3E3', marginBottom: '5px', display:'flex', justifyContent:'space-between' }}>
-        <h1 style={{alignContent:'center',display:'flex', }}>Files</h1>
+      <div style={{ backgroundColor: '#E3E3E3', marginBottom: '5px', display:'flex', justifyContent:'space-between', }}>
+        <h1 style={{alignContent:'center',display:'flex', margin:'0px 20px' }}>Files</h1>
         <button style={{
           width: '70px',
           height: '20px',
@@ -69,6 +69,7 @@ const FileList = ({ projectId}) => {
           fontSize:'12px',
           background: 'linear-gradient(to bottom, #30b546, #47FF66)',
           boxShadow: '0px 2px 1px #30b546',
+          margin:'0px 20px'
         }} onClick={handleOpenUploadPopup}>Upload</button>
       </div>
       {showUploadPopup && (
@@ -76,57 +77,60 @@ const FileList = ({ projectId}) => {
                         <FileUpload projectId={projectId} onClose={handleCloseUploadPopup}/>   
                         </div>
                         )} 
-      <table style={{ borderCollapse: 'collapse', width: '100%' }}>
-        <tbody>
-          {console.log(files)}
-          {files.map((file, index) => (
-            <tr key={file.fileId} style={{ borderBottom: '1px solid #ccc' }}>
-              {console.log(file.fileId)}
-              <td style={{ padding: '5px 0' }}>
-                <span>{file.name}</span>
-              </td>
-              <td style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <button
-                  onClick={() => handleDownload(file.url)}
-                  style={{
-                    backgroundColor: '#6B9EFF',
-                    color: 'white',
-                    border: 'none',
+      <div style={{ maxHeight: '110px', overflowY: 'auto' }}>  
+        <table style={{ borderCollapse: 'collapse', width: '100%', alignContent:'center' }}>
+          <tbody>
+            {console.log(files)}
+            {files.map((file, index) => (
+              <tr key={file.fileId} style={{ borderBottom: '1px solid #ccc' }}>
+                {console.log(file.fileId)}
+                <td style={{ padding: '5px 0' }}>
+                  <span>{file.name}</span>
+                </td>
+                <td style={{ display: 'flex', justifyContent: 'flex-end', }}>
+                
+                  <button
+                    onClick={() => handleDownload(file.url)}
+                    style={{
+                      backgroundColor: '#6B9EFF',
+                      color: 'white',
+                      border: 'none',
 
-                    borderRadius: '5px',
-                    marginRight: '10px',
-                    textAlign: 'center',
-                    lineHeight: '20px',
-                    cursor: 'pointer',
-                    transition: 'background-color 0.3s ease',
-                    width: '70px',
-                    height: '30px',
-                    fontSize:'12px'
-                  }}
-                >
-                  Download
-                </button>
-                <button
-                  onClick={() => handleDelete(file.url, file.fileId)}
-                  style={{
-                    backgroundColor: '#EB465A',
-                    color: 'white',
-                    border: 'none',
-                    fontSize:'12px',
-                    borderRadius: '5px',
-                    cursor: 'pointer',
-                    transition: 'background-color 0.3s ease',
-                    width: '70px',
-                    height: '30px'
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                      borderRadius: '5px',
+                      marginRight: '10px',
+                      textAlign: 'center',
+                      lineHeight: '20px',
+                      cursor: 'pointer',
+                      transition: 'background-color 0.3s ease',
+                      width: '70px',
+                      height: '30px',
+                      fontSize:'12px'
+                    }}
+                  >
+                    Download
+                  </button>
+                  <button
+                    onClick={() => handleDelete(file.url, file.fileId)}
+                    style={{
+                      backgroundColor: '#EB465A',
+                      color: 'white',
+                      border: 'none',
+                      fontSize:'12px',
+                      borderRadius: '5px',
+                      cursor: 'pointer',
+                      transition: 'background-color 0.3s ease',
+                      width: '70px',
+                      height: '30px'
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>  
     </div>
   );
 };
